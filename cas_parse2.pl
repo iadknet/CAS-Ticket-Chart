d129a4b8deed91d0d03aa8b44945134de8e26ff7
Isaac Stefanek <isaac@iadk.net>
2014-07-16 17:32:07 -0700

    Cleaned up formatting

--- cas_parse2.pl
+++ cas_parse2.pl
@@ -9,9 +9,9 @@ my %global_service_counter;
 while(my $line = <STDIN>)
 {
 
-    # 2012-09-12 14:36:43,117 INFO [org.jasig.cas.CentralAuthenticationServiceImpl] - Granted service ticket [ST-720745-dFo5g7d5Bvl1gW5X3FxF-cas] for service [https://my.csumb.edu/] for user [appa9057]
+  # 2012-09-12 14:36:43,117 INFO [org.jasig.cas.CentralAuthenticationServiceImpl] - Granted service ticket [ST-720745-dFo5g7d5Bvl1gW5X3FxF-cas] for service [https://my.csumb.edu/] for user [appa9057]
 
-    # $1 - day $2 - time $3 - service $4 - user
+  # $1 - day $2 - time $3 - service $4 - user
 	if($line =~ m/(^\d{4}-\d{2}-\d{2}) (\d{2}:\d{2}:\d{2}),\d+ INFO \[org.jasig.cas.CentralAuthenticationServiceImpl\] - Granted service ticket \[[^\]]*] for service \[([^\]]*)] for user \[([^\]]*)]/)
 	{
 
@@ -32,8 +32,6 @@ while(my $line = <STDIN>)
 
 	}
 }
-
-
 
 
 print '{' . "";
@@ -70,7 +68,7 @@ foreach my $day (sort keys %days)
 	$day_of_month =~ s/^0+//;
 
 	print '{"c":[{"v":"Date(' . "$year, $month, $day_of_month" . ')","f":null},' . "";
-	
+
 	#print "$day,";
 	my $i = 0;
  	foreach my $service (sort {$global_service_counter{$b} <=> $global_service_counter{$a}} keys %global_service_counter)
@@ -83,14 +81,14 @@ foreach my $day (sort keys %days)
  		{
  			$count = 0;
  		}
- 		
+
  		print '{"v":' . $count . ',"f":null}' . "";
  		if ($i != keys %global_service_counter)
 		{
 			print ',' . "";
 		}
 
- 		
+
  	}
  	print ']}' . "";
  	if ($dcount != $total_day_count)
@@ -98,11 +96,8 @@ foreach my $day (sort keys %days)
  		print ',' . "";
  	}
 
- 	
 
 }
-
-
 
 print ']' . "";
 print '}' . "";
